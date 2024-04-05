20fba7794b93c4f73987563d52bfbf97f225d831
VessyPenkova <99133121+VessyPenkova@users.noreply.github.com>
2024-04-05 21:48:42 +0300

    WebApi Added

---
+++ CarRentitngSystem.WebApi/Controllers/Api/StatisticsApiController.cs
@@ -0,0 +1,32 @@
+﻿using CarRentingSystem.Core.Contracts.Statistics;
+using CarRentingSystem.Core.Models.Statistics;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarRentitngSystem.WebApi.Controllers.Api
+{
+    [Route("api/statistics")]
+    [ApiController]
+    public class StatisticsApiController : ControllerBase
+    {
+        private readonly IStatisticsService service;
+        public StatisticsApiController(IStatisticsService _service)
+        {
+            this.service = _service;
+        }
+        /// <summary>
+        /// Get statistics about count of rented cars and available routes
+        /// </summary>
+        /// <returns>Total routes and  and booked shipments</returns>
+        [HttpGet]
+        [Produces("application/json")]
+        [ProducesResponseType(200,Type = typeof(StatisticsServiceModel))]
+        [ProducesResponseType(500)]
+        public async Task<IActionResult> GetStatistics()
+        {
+            var model = await service.Total();
+
+            return Ok(model);
+        }
+    }
+}
---
+++ CarRentitngSystem.WebApi/Extensions/CarRentingApiServiceCollectionExtension.cs
@@ -0,0 +1,32 @@
+﻿using CarRentingSystem.Core.Contracts.Statistics;
+using CarRentingSystem.Core.Exceptions;
+using CarRentingSystem.Core.Services.Statistic;
+using CarRentingSystem.Infrastucture.Data;
+using CarRentingSystem.Infrastucture.Data.Common;
+using Microsoft.EntityFrameworkCore;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class CarRentingApiServiceCollectionExtension
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddScoped<IRepository, Repository>();
+            services.AddScoped<IStatisticsService, StatisticsService>();
+  
+            services.AddScoped<IGuard, Guard>();
+
+            return services;
+        }
+
+        public static IServiceCollection AddApplicationDbContext(this IServiceCollection services, IConfiguration config)
+        {
+            var connectionString = config.GetConnectionString("DefaultConnection");
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(connectionString));
+            services.AddScoped<IRepository, Repository>();
+
+            return services;
+        }
+    }
+}
